cb5fada3033a165846b8324056172d4487e90134
GeorgeSeiras <46285035+GeorgeSeiras@users.noreply.github.com>
2020-07-12 13:26:12 +0300

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import * as mongoose from 'mongoose';
 import * as morgan from 'morgan';
 import * as cors from 'cors';
 import * as session from 'express-session';
+import { router } from './routes/index';
 import './models/Table';
 import './models/Order';
 import './models/User';
@@ -16,30 +17,32 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/", require('./routes'));
-app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+app.use("/", router);
+app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 app.use(function (err: express.ErrorRequestHandler, req: express.Request, res: express.Response, next: express.NextFunction) {
     if (err.name === 'UnauthorizedError') {
         res.status(401).send('invalid token...');
     }
 });
-app.use(function(req, res, next) {
-    let err:any = new Error('Not Found');
+app.use(function (req, res, next) {
+    let err: any = new Error('Not Found');
     err.status = 404;
     next(err);
-  });
+});
 
-  app.use(function(err:any, req:express.Request, res:express.Response, next:express.NextFunction) {
+app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
     console.log(err.stack);
-  
+
     res.status(err.status || 500);
-  
-    res.json({'errors': {
-        message: err.message,
-        error: err
-      }});
-  });
+
+    res.json({
+        'errors': {
+            message: err.message,
+            error: err
+        }
+    });
+});
 const connection = 'mongodb+srv://user:user@cluster0.wsqb7.mongodb.net/Caffe?retryWrites=true&w=majority';
 mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
 
